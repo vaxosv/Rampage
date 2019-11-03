f10cbf96e416aa49d0aadf996363a44e6bc78f2b
vaxosv <vaxo.sv@gmail.com>
2019-11-03 14:02:15 +0400

    npm serve

--- src/app.ts
+++ src/app.ts
@@ -10,27 +10,12 @@ let stop = false;
 
 const pixi = new Pixi();
 const header = new TexT(100, 10, 'slot');
-const header2 = new TexT(200, 10, 'slot2');
-const spell = new SpritE(300, 10);
 pixi.app.stage.addChild(header.text);
-pixi.app.stage.addChild(header2.text);
-pixi.app.stage.addChild(spell.sprite);
 
 
-// for (let i = 0; i <= 10; i++) {
-//     pixi.app.stage.addChild(header.text);
-//     arr.push(header)
-//
-//     const header = new TexT('slot', 100 * i, 10);
-// }
 console.log(pixi.app);
 pixi.app.ticker.add((delta) => {
     header.update(delta, speed);
-    header2.update(delta, speed);
-    spell.update(delta, speed);
-    // arr.forEach(item => {
-    //     item.update(delta, 5)
-    // })
 });
 
 
